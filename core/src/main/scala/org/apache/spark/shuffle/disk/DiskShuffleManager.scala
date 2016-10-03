2735713de7e4941f467c066ef4d35b07f5aa2194
ooq <qifan.pu@gmail.com>
2016-10-03 12:45:39 -0700

    Add disk shuffle files.

---
+++ core/src/main/scala/org/apache/spark/shuffle/disk/DiskShuffleManager.scala
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.shuffle.disk
+
+import java.util.concurrent.ConcurrentHashMap
+
+import org.apache.spark.{ShuffleDependency, SparkConf, TaskContext}
+import org.apache.spark.shuffle._
+
+
+private[spark] class DiskShuffleManager(conf: SparkConf) extends ShuffleManager {
+  private[this] val numMapsForShuffle = new ConcurrentHashMap[Int, Int]()
+
+  override val shuffleBlockResolver = new DiskShuffleBlockResolver(conf)
+
+  override def registerShuffle[K, V, C](
+                                         shuffleId: Int,
+                                         numMaps: Int,
+                                         dependency: ShuffleDependency[K, V, C]): ShuffleHandle = {
+    // println("register shuffle for disk manager")
+    new BaseShuffleHandle(shuffleId, numMaps, dependency)
+  }
+
+  override def getReader[K, C](
+                                handle: ShuffleHandle,
+                                startPartition: Int,
+                                endPartition: Int,
+                                context: TaskContext): ShuffleReader[K, C] = {
+    new BlockStoreShuffleReader(
+      handle.asInstanceOf[BaseShuffleHandle[K, _, C]], startPartition, endPartition, context)
+  }
+
+  override def getWriter[K, V](handle: ShuffleHandle, mapId: Int,
+                               context: TaskContext): ShuffleWriter[K, V] = {
+    new DiskShuffleWriter(
+      shuffleBlockResolver, handle.asInstanceOf[BaseShuffleHandle[K, V, _]], mapId, context)
+  }
+
+  override def unregisterShuffle(shuffleId: Int): Boolean = {
+    shuffleBlockResolver.removeShuffle(shuffleId)
+  }
+
+  override def stop(): Unit = {
+    shuffleBlockResolver.stop()
+  }
+}
